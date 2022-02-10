aeda3a27621cf4904f8c178f2ea92b2a4b1f74ae
kgandhi09 <kushal009gandhi@gmail.com>
2022-02-10 00:11:46 -0500

    changing the approach, trying raycasting for depth estimation

---
+++ Assets/Scripts/Sensors/rgb_d.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+///     This script simulates a laser scanner to
+///     detect the surrounding obstacles
+/// </summary>
+public class rgb_d : MonoBehaviour
+{
+ 
+    void Start()
+    {
+	//Getting the camera object for the ray origin
+	Camera cam = gameObject.GetComponent<Camera>();
+	
+    }
+
+    void Update()
+    {
+
+    }
+
+}
